6767f50a7aeafde38e2413cd42cda565317314a0
shilpijadoun <rajputshilpi141@gmail.com>
2024-10-14 10:36:53 +0530

    update

---
+++ DAY5/script.js
@@ -0,0 +1,90 @@
+// function display()
+// {
+    // alert ("hello js");
+// }
+// let x=12;
+// function display(){
+//     console.log("hello js",x);
+// }
+
+// display();
+
+// let x=12.33;
+// let y="hello"
+// let z=true;
+
+// //OBJECT...
+// let obj={
+//     'name':'Shilpi',
+//     id: 12,
+
+// }
+// let ar=[12,3,5,4];
+// let a;
+// let b=null;
+// console.log(typeof b);
+
+//create function........
+
+function validate(){
+    let un=document.getElementById('un').Value;
+    let pass=document.getElementById('pass').Value;
+    if(un=='admin' && pass=='admin')
+    {
+     document.getElementById("res").innerHTML ="Login Success";   
+
+    }
+    else
+    document.getElementById("res").innerHTML ="Login Failed";
+}
+
+// let x=Number("12");
+// let str=("123");
+// console.log(x+str);
+
+// "use strict"
+// let num=12;
+// console.log(num);
+
+// let a=12;
+// let b='12';
+// console.log(a==b);
+// console.log(a===b);
+
+
+//WITHOUT PARAMETER..........
+// let a=()=>{
+//     console.log("hello");
+// };
+// a();
+
+// let b=()=>{
+//     console.log("hii");
+// };
+// b();
+
+
+//TWO PARAMETER...........
+// let a=(x,y)=>{
+//     return (x+y);
+//     // console.log(x+y);
+// };
+// let x=a(12,34);
+// console.log(x);
+// a(12,14);
+
+//SINGLE LINE ARROW FUNCTION.........
+// let say=()=> console.log("hello");
+// say();
+// function display(){
+//   let p1 = document.querySelector("p");
+//   p1.innerHTML = "Result";
+//   p1.style.backgroundColor = "red";
+
+// }
+
+
+
+
+
+
